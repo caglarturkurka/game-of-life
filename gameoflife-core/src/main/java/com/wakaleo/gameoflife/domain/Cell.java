25bd98970c4c0db591819f689ed639ce87abb6f6
caglarturkurka <caglarturkurka@gmail.com>
2015-07-01 23:29:35 +0300

    Cell was changed

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,31 +1,30 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("*"), DEAD_CELL(".");
+	LIVE_CELL("+"), DEAD_CELL(".");
+	private String symbol;
 
-    private String symbol;
+	private Cell(String symbol) {
+		this.symbol = symbol;
+	}
 
-    private Cell(final String initialSymbol) {
-        this.symbol = initialSymbol;
-    }
+	@Override
+	public String toString() {
+		return symbol;
+	}
 
-    @Override
-    public String toString() {
-        return symbol;
-    }
+	static Cell fromSymbol(String symbol) {
+		Cell cellRepresentedBySymbol = null;
+		for (Cell cell : Cell.values()) {
+			if (cell.symbol.equals(symbol)) {
+				cellRepresentedBySymbol = cell;
+				break;
+			}
+		}
+		return cellRepresentedBySymbol;
+	}
 
-    static Cell fromSymbol(final String symbol) {
-        Cell cellRepresentedBySymbol = null;
-        for (Cell cell : Cell.values()) {
-            if (cell.symbol.equals(symbol)) {
-                cellRepresentedBySymbol = cell;
-                break;
-            }
-        }
-        return cellRepresentedBySymbol;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
+	public String getSymbol() {
+		return symbol;
+	}
 }
